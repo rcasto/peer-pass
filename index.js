157a7d8c5388408c1a62885e9812df1b384d570c
Richie Casto <rcasto92@gmail.com>
2021-05-29 22:40:04 -0400

    add some simple initial request validation

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+function isValidRequest(req) {
+    if (req.method !== 'POST') {
+        return false;
+    }
+    
+    const contentType = req.get('content-type');
+    if (contentType !== 'application/json') {
+        return false;
+    }
+
+    if (!req.body) {
+        return false;
+    }
+
+    return true;
+}
+
 /**
  * HTTP Cloud Function.
  *
@@ -7,5 +24,10 @@
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.connectPeer = (req, res) => {
+    if (!isValidRequest(req)) {
+        res.status(400).end();
+        return;
+    }
+
     res.send(`Hello World!`);
 };
