155414ac8be1eb66b074d1a041f39089dd5ba796
Richie Casto <rcasto92@gmail.com>
2021-06-05 19:51:03 -0400

    stash

--- functions/createRoom.js
+++ functions/createRoom.js
@@ -1,3 +1,17 @@
+/**
+ * Proposed room schema:
+ * {
+ *     name: string;
+ *     password?: string;
+ *     offers: string[];
+ * }
+ */
+
+/**
+ * Questions:
+ * - How does a room remain active?
+ * - How is contention resolved for offers? Blob storage won't handle this. 2 peers could grab the same offer.
+ */
 exports.createRoom = (req, res) => {
 
 };
