1eae697be0e9deceb3eb01182fe5060aaa9d229e
Richie Casto <rcasto92@gmail.com>
2021-07-09 22:00:19 -0400

    add max cycle amount when generating unique one time code

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -3,6 +3,7 @@ import { ErrorWrapper, WrapErrorInput } from '../schemas';
 
 const ONE_TIME_CODE_LENGTH: number = 6;
 const MAX_ONE_TIME_CODE: number = parseInt('9'.repeat(ONE_TIME_CODE_LENGTH), 10) + 1;
+const MAX_ONE_TIME_CODE_GENERATE_ATTEMPTS: number = 25;
 
 function generateOneTimeCode(): string {
     const randomNumString = randomInt(MAX_ONE_TIME_CODE).toString(10);
@@ -11,12 +12,18 @@ function generateOneTimeCode(): string {
 
 export async function generateUniqueOneTimeCode(isCodeUnique: (key: string) => Promise<boolean>): Promise<string> {
     let isCodeAlreadyUsed: boolean  = false;
+    let numCycles: number = 0;
     let code: string;
 
     do {
         try {
+            if (numCycles > MAX_ONE_TIME_CODE_GENERATE_ATTEMPTS) {
+                throw new Error('Max cycles reached when attempting to generate a unique one time code.');
+            }
+
             code = generateOneTimeCode();
             isCodeAlreadyUsed = await isCodeUnique(code);
+            numCycles++;
         } catch (err) {
             throw wrapError({
                 source: generateUniqueOneTimeCode,
